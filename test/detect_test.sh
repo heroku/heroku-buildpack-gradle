fc6d2f7256c4114a7b30b388e628cccbd79e1ff6
David M. Carr <dcarr@commercehub.com>
2014-03-19 09:50:58 -0400

    detect: support projects with non-default build file names
    With Gradle, it's possible to have a valid project where no file named `build.gradle` is used.
    For these projects, you set the `buildFileName` on the `ProjectDescriptor` in your `settings.gradle`.
    The Ratpack project is currently an example of this type of project.
    Thus, I've changed the detect logic to consider an application as a Gradle application when either
    `build.gradle` or `settings.gradle` is found.
    
    http://www.gradle.org/docs/current/dsl/org.gradle.api.initialization.Settings.html
    http://www.gradle.org/docs/current/javadoc/org/gradle/api/initialization/ProjectDescriptor.html
    https://github.com/ratpack/ratpack

--- test/detect_test.sh
+++ test/detect_test.sh
@@ -2,7 +2,16 @@
 
 . ${BUILDPACK_TEST_RUNNER_HOME}/lib/test_utils.sh
 
-testDetect()
+testDetechSettingsGradle()
+{
+  touch ${BUILD_DIR}/settings.gradle
+
+  detect
+
+  assertAppDetected "Gradle"
+}
+
+testDetectBuildGradle()
 {
   touch ${BUILD_DIR}/build.gradle
   
@@ -11,7 +20,7 @@ testDetect()
   assertAppDetected "Gradle"
 }
 
-testNoDetectMissingBuildGradle()
+testNoDetectMissingBuildGradleAndSettingsGradle()
 {
   touch ${BUILD_DIR}/build.xml
 
