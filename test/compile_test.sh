4bea624f9412dc20a1cf5acd55461a8470a7f2f3
Joe Kutner <jpkutner@gmail.com>
2014-08-26 16:12:11 -0500

    move wrapper in opt so it can be used as a default in the compile phase

--- test/compile_test.sh
+++ test/compile_test.sh
@@ -2,19 +2,26 @@
 
 . ${BUILDPACK_TEST_RUNNER_HOME}/lib/test_utils.sh
 
-WRAPPER_DIR=${BUILDPACK_HOME}/test/wrapper
-  
+WRAPPER_DIR=${BUILDPACK_HOME}/opt/wrapper
+
 installWrapper() {
   cp -r "$WRAPPER_DIR"/* ${BUILD_DIR}
 }
 
 testCompileWithoutWrapper()
 {
-  compile 
-  assertCapturedError "'./gradlew' script not found - please enable the Gradle Wrapper for this project."
+  cat > ${BUILD_DIR}/build.gradle <<EOF
+task stage << {
+  println "${expected_stage_output}"
+}
+EOF
+
+  compile
+  assertCapturedSuccess
+  assertCaptured "Installing Gradle Wrapper"
 }
 
-testCompileWithWrapper() 
+testCompileWithWrapper()
 {
   installWrapper
   expected_stage_output="STAGING:${RANDOM}"
@@ -27,12 +34,12 @@ EOF
 
   compile
   assertCapturedSuccess
-  assertCaptured "Installing OpenJDK 1.6" 
-  assertCaptured "${expected_stage_output}" 
+  assertCaptured "Installing OpenJDK 1.6"
+  assertCaptured "${expected_stage_output}"
   assertCaptured "BUILD SUCCESSFUL"
   assertTrue "Java should be present in runtime." "[ -d ${BUILD_DIR}/.jdk ]"
   assertTrue "Java version file should be present." "[ -f ${BUILD_DIR}/.jdk/version ]"
-  assertTrue "System properties file should be present in build dir." "[ -f ${BUILD_DIR}/system.properties ]" 
+  assertTrue "System properties file should be present in build dir." "[ -f ${BUILD_DIR}/system.properties ]"
   assertTrue "Gradle profile.d file should be present in build dir." "[ -f ${BUILD_DIR}/.profile.d/gradle.sh ]"
   assertTrue "GRADLE_USER_HOME should be CACHE_DIR/.gradle." "[ -d ${CACHE_DIR}/.gradle ]"
 }
@@ -59,15 +66,15 @@ EOF
 testCompile_Fail()
 {
   installWrapper
-  expected_stage_output="STAGING:${RANDOM}"  
-  
+  expected_stage_output="STAGING:${RANDOM}"
+
   cat > ${BUILD_DIR}/build.gradle <<EOF
 task stage << {
   throw new GradleException("${expected_stage_output}")
 }
 EOF
 
-  compile  
+  compile
   assertCapturedError "${expected_stage_output}"
   assertCapturedError "BUILD FAILED"
 }
