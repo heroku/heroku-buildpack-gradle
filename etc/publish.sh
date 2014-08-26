e6c2297229d3a28052d24a56264135e7313702b5
Joe Kutner <jpkutner@gmail.com>
2014-08-26 16:22:49 -0500

    added some helper scripts [skip ci]

---
+++ etc/publish.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ ! -z "$1" ]; then
+  pushd . > /dev/null 2>&1
+  cd /tmp &&
+  git clone git@github.com:heroku/heroku-buildpack-gradle.git &&
+  cd heroku-buildpack-gradle &&
+  git checkout master &&
+  heroku buildpacks:publish $1/gradle
+  popd > /dev/null 2>&1
+  echo "Cleaning up..."
+  rm -rf /tmp/heroku-buildpack-gradle
+  echo "Done."
+else
+  echo "You must provide a buildkit organization as an argument!"
+  exit 1
+fi
