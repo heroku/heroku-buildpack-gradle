743f73cbc9326e7865cd26e37ba6f4b4e1e5f2df
Joe Kutner <jpkutner@gmail.com>
2014-09-25 10:18:00 -0500

    improvement to publish script [skip ci]

--- etc/publish.sh
+++ etc/publish.sh
@@ -6,7 +6,7 @@ if [ ! -z "$1" ]; then
   git clone git@github.com:heroku/heroku-buildpack-gradle.git &&
   cd heroku-buildpack-gradle &&
   git checkout master &&
-  find . ! -name '.' ! -name '..' ! -name 'bin' -maxdepth 1 -print0 | xargs -0 rm -rf -- &&
+  find . ! -name '.' ! -name '..' ! -name 'bin' ! -name 'opt' -maxdepth 1 -print0 | xargs -0 rm -rf -- &&
   heroku buildpacks:publish $1/gradle
   popd > /dev/null 2>&1
   echo "Cleaning up..."
