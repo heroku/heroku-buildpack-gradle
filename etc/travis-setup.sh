13fa1fe7c17e71240675893589715b5b88450da6
Joe Kutner <jpkutner@gmail.com>
2017-02-08 11:51:00 -0600

    Updated travis config

--- etc/travis-setup.sh
+++ etc/travis-setup.sh
@@ -1,12 +1,14 @@
-if [[ "$TEST_CMD" =~ "sh etc/hatchet-test.sh" ]] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
-  if [ -n "`git config --get user.email`" ]; then
-    echo 'already set'; else `git config --global user.email 'buildpack@example.com'`
-  fi
-  if [ -n "`git config --get user.name`" ]; then
-    echo 'already set'; else `git config --global user.name 'BuildpackTester'`
-  fi
+#!/usr/bin/env bash
 
-  cat <<EOF > ~/.ssh/config
+[ "$TRAVIS" != "true" ] && echo "Not running on Travis!" && exit 1
+
+curl --silent https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/shunit2/shunit2-2.1.6.tgz | tar xz -C /tmp/
+git clone https://github.com/heroku/heroku-buildpack-testrunner.git /tmp/testrunner
+
+git config --global user.email ${HEROKU_API_USER:-"buildpack@example.com"}
+git config --global user.name 'BuildpackTester'
+
+cat <<EOF >> ~/.ssh/config
 Host heroku.com
     StrictHostKeyChecking no
     CheckHostIP no
@@ -15,7 +17,12 @@ Host github.com
     StrictHostKeyChecking no
 EOF
 
-  curl --fail --retry 3 --retry-delay 1 --connect-timeout 3 --max-time 30 https://toolbelt.heroku.com/install-ubuntu.sh | sh
+cat <<EOF >> ~/.netrc
+machine git.heroku.com
+  login ${HEROKU_API_USER:-"buildpack@example.com"}
+  password ${HEROKU_API_KEY:-"password"}
+EOF
 
-  yes | heroku keys:add
-fi
+curl --fail --retry 3 --retry-delay 1 --connect-timeout 3 --max-time 30 https://toolbelt.heroku.com/install-ubuntu.sh | sh
+
+yes | heroku keys:add
