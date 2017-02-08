021b349260702f56174b563f6e82b3790c630283
Joe Kutner <jpkutner@gmail.com>
2017-02-08 10:16:18 -0600

    Updating travis configuration

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source "https://rubygems.org"
+
+gem 'heroku_hatchet', '~> 1.4.2'
+gem 'rspec-retry'
+gem 'rspec-expectations'
--- etc/hatchet-test.sh
+++ etc/hatchet-test.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
-gem install rspec-core -v 3.5.2
-gem install rspec-retry -v 0.4.0
-gem install rspec-expectations -v 3.5.0
-gem install rspec-mocks -v 3.5.0
-gem install heroku_hatchet -v 1.4.1
+if [ "$TRAVIS" = "true" ] && [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ -n "$HEROKU_API_KEY" ]; then
+  echo "Skipping Hatchet tests on Pull Request."
+else
+  gem install bundler
+  bundle install
 
-hatchet install &&
-HATCHET_RETRIES=3 \
-HATCHET_DEPLOY_STRATEGY=git \
-HATCHET_BUILDPACK_BASE="https://github.com/heroku/heroku-buildpack-gradle.git" \
-HATCHET_BUILDPACK_BRANCH=$(git name-rev HEAD 2> /dev/null | sed 's#HEAD\ \(.*\)#\1#') \
-rspec $@
+  bundle exec hatchet install &&
+  HATCHET_RETRIES=3 \
+  HATCHET_DEPLOY_STRATEGY=git \
+  HATCHET_BUILDPACK_BASE="https://github.com/heroku/heroku-buildpack-gradle.git" \
+  HATCHET_BUILDPACK_BRANCH=$(git name-rev HEAD 2> /dev/null | sed 's#HEAD\ \(.*\)#\1#') \
+  bundle exec rspec spec/
+fi
