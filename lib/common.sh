d75e8a6adb35bb86e7e4a43d527b737fe72c3ce3
Manuel Fuchs <manuel.fuchs@salesforce.com>
2025-09-05 14:39:39 +0200

    Replace custom output function with tee and output::indent

--- lib/common.sh
+++ lib/common.sh
@@ -11,18 +11,6 @@ gradle_build_file() {
 	fi
 }
 
-# sed -l basically makes sed replace and buffer through stdin to stdout
-# so you get updates while the command runs and dont wait for the end
-# e.g. sbt stage | indent
-output() {
-	local logfile="${1}"
-	local c='s/^/       /'
-
-	case $(uname) in
-		Darwin) tee -a "${logfile}" | sed -l "${c}";; # mac/bsd sed: -l buffers on line boundaries
-		*)      tee -a "${logfile}" | sed -u "${c}";; # unix/gnu sed: -u unbuffered (arbitrary) chunks of data
-	esac
-}
 
 cache_copy() {
 	rel_dir="${1}"
