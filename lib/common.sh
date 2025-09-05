ba152ea1ad4f72f7855fccc8aed54c0e7a90a27a
Manuel Fuchs <manuel.fuchs@salesforce.com>
2025-09-05 15:41:46 +0200

    Use new cache_copy function

--- lib/common.sh
+++ lib/common.sh
@@ -12,18 +12,6 @@ gradle_build_file() {
 }
 
 
-cache_copy() {
-	rel_dir="${1}"
-	from_dir="${2}"
-	to_dir="${3}"
-	rm -rf "${to_dir}/${rel_dir}"
-	if [ -d "${from_dir}/${rel_dir}" ]; then
-		mkdir -p "${to_dir}/${rel_dir}"
-		cp -pr "${from_dir}/${rel_dir}"/. "${to_dir}/${rel_dir}"
-	fi
-}
-
-
 # Detects if the application has a custom 'stage' task defined in the build file.
 # This is the preferred way to define the build task for Heroku deployment.
 has_stage_task() {
