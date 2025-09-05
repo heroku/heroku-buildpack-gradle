c2c8eac35cbd0b36debaee1d5cde9c004ed423b0
Manuel Fuchs <manuel.fuchs@salesforce.com>
2025-09-05 18:43:37 +0200

    Add testing infrastructure

---
+++ Gemfile
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
+
+ruby '>= 3.2', '< 3.5'
+
+group :test, :development do
+  gem 'heroku_hatchet'
+  # Work around https://github.com/excon/excon/issues/860
+  gem 'logger'
+  gem 'parallel_split_test'
+  gem 'rspec-core'
+  gem 'rspec-expectations'
+  gem 'rspec-retry'
+  gem 'rubocop', require: false
+  gem 'rubocop-rspec', require: false
+end
---
+++ test/spec/spec_helper.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+require 'rspec/core'
+require 'hatchet'
+require 'fileutils'
+require 'rspec/retry'
+require 'date'
+
+ENV['RACK_ENV'] = 'test'
+ENV['HATCHET_BUILDPACK_BASE'] ||= 'https://github.com/heroku/heroku-buildpack-gradle.git'
+
+RSpec.configure do |config|
+  # Disables the legacy rspec globals and monkey-patched `should` syntax.
+  config.disable_monkey_patching!
+  # Enable flags like --only-failures and --next-failure.
+  config.example_status_persistence_file_path = '.rspec_status'
+  # Allows limiting a spec run to individual examples or groups by tagging them
+  # with `:focus` metadata via the `fit`, `fcontext` and `fdescribe` aliases.
+  config.filter_run_when_matching :focus
+  # Allows declaring on which stacks a test/group should run by tagging it with `stacks`.
+  config.filter_run_excluding stacks: ->(stacks) { !stacks.include?(ENV.fetch('HATCHET_DEFAULT_STACK')) }
+  # Make rspec-retry output a retry message when its had to retry a test.
+  config.verbose_retry = true
+end
+
+def successful_body(app, options = {})
+  retry_limit = options[:retry_limit] || 50
+  path = options[:path] ? "/#{options[:path]}" : ''
+  Excon.get("#{app.platform_api.app.info(app.name).fetch('web_url')}#{path}", idempotent: true, expects: 200,
+                                                                              retry_limit: retry_limit).body
+end
+
+def set_java_version(directory, version)
+  write_sys_props directory, "java.runtime.version=#{version}"
+end
+
+def write_sys_props(directory, props)
+  Dir.chdir(directory) do
+    `rm -f system.properties`
+    File.open('system.properties', 'w') do |f|
+      f.puts props
+    end
+    `git add system.properties && git commit -m "setting jdk version"`
+  end
+end
+
+def clean_output(output)
+  output
+    # Remove trailing whitespace characters added by Git:
+    # https://github.com/heroku/hatchet/issues/162
+    .gsub(/ {8}(?=\R)/, '')
+    # Remove ANSI colour codes used in buildpack output (e.g. error messages).
+    .gsub(/\e\[[0-9;]+m/, '')
+end
