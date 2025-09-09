607d5f1b8964d1ee6091bb108ed034775a888291
Manuel Fuchs <manuel.fuchs@salesforce.com>
2025-09-09 15:05:37 +0200

    Add Quarkus tests

---
+++ test/spec/fixtures/repos/quarkus-3-gradle-kotlin/settings.gradle.kts
@@ -0,0 +1,15 @@
+package spec.fixtures.repos.`quarkus-3-gradle-kotlin`
+
+pluginManagement {
+    val quarkusPluginVersion: String by settings
+    val quarkusPluginId: String by settings
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+        mavenLocal()
+    }
+    plugins {
+        id(quarkusPluginId) version quarkusPluginVersion
+    }
+}
+rootProject.name="code-with-quarkus"
---
+++ test/spec/quarkus_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require_relative 'spec_helper'
+
+RSpec.describe 'Gradle buildpack' do
+  it 'can build and run Quarkus app with Kotlin DSL build script' do
+    app = Hatchet::Runner.new('quarkus-3-gradle-kotlin')
+    app.deploy do
+      # Should detect Quarkus and run build -x check by default
+      expect(app.output).to include('$ ./gradlew build -x check')
+      expect(app.output).to include('BUILD SUCCESSFUL')
+
+      expect(successful_body(app)).to include('Hello Heroku!')
+    end
+  end
+end
