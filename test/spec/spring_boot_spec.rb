df03460367e0183e456e7d2e16a15cb503d4eb6e
Manuel Fuchs <manuel.fuchs@salesforce.com>
2025-09-09 10:43:37 +0200

    Add Spring boot integration tests

---
+++ test/spec/spring_boot_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require_relative 'spec_helper'
+
+RSpec.describe 'Gradle buildpack' do
+  it 'can build and run Spring Boot app with Groovy DSL build script' do
+    app = Hatchet::Runner.new('spring-3-gradle-groovy')
+    app.deploy do
+      # Should detect Spring Boot and run build -x check by default
+      expect(app.output).to include('$ ./gradlew build -x check')
+      expect(app.output).to include('BUILD SUCCESSFUL')
+
+      expect(successful_body(app)).to include('Hello Heroku!')
+    end
+  end
+
+  it 'can build and run Spring Boot app with Kotlin DSL build script' do
+    app = Hatchet::Runner.new('spring-3-gradle-kotlin')
+    app.deploy do
+      # Should detect Spring Boot and run build -x check by default
+      expect(app.output).to include('$ ./gradlew build -x check')
+      expect(app.output).to include('BUILD SUCCESSFUL')
+
+      expect(successful_body(app)).to include('Hello Heroku!')
+    end
+  end
+end
