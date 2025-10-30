715232defee375a0ee968393f63c42ab77a451b2
Manuel Fuchs <manuel.fuchs@salesforce.com>
2025-10-30 13:21:21 +0100

    Fix trailing whitespace in output functions (#180)

--- test/spec/detection_spec.rb
+++ test/spec/detection_spec.rb
@@ -9,28 +9,28 @@ RSpec.describe 'Gradle buildpack detection' do
       expect(clean_output(app.output)).to include(<<~OUTPUT)
         remote:  !     Error: Your app is configured to use the Gradle buildpack,
         remote:  !     but we couldn't find any supported Gradle project files.
-        remote:  !     
+        remote:  !
         remote:  !     The Gradle buildpack requires at least one of the following files
         remote:  !     in the root directory of your source code:
-        remote:  !     
+        remote:  !
         remote:  !     Supported Gradle files: gradlew, build.gradle, build.gradle.kts, settings.gradle, settings.gradle.kts
-        remote:  !     
+        remote:  !
         remote:  !     IMPORTANT: If your project uses a different build tool:
         remote:  !     - For Maven projects, use the heroku/java buildpack instead
         remote:  !     - For sbt projects, use the heroku/scala buildpack instead
-        remote:  !     
+        remote:  !
         remote:  !     Currently the root directory of your app contains:
-        remote:  !     
+        remote:  !
         remote:  !     README.md
-        remote:  !     
+        remote:  !
         remote:  !     If your app already has Gradle files, check that they:
-        remote:  !     
+        remote:  !
         remote:  !     1. Are in the top level directory (not a subdirectory).
         remote:  !     2. Have the correct spelling (the filenames are case-sensitive).
         remote:  !     3. Aren't listed in '.gitignore' or '.slugignore'.
         remote:  !     4. Have been added to the Git repository using 'git add --all'
         remote:  !        and then committed using 'git commit'.
-        remote:  !     
+        remote:  !
         remote:  !     For help with using Gradle on Heroku, see:
         remote:  !     https://devcenter.heroku.com/articles/deploying-gradle-apps-on-heroku
       OUTPUT
--- test/spec/misc_spec.rb
+++ test/spec/misc_spec.rb
@@ -14,23 +14,23 @@ RSpec.describe 'Gradle buildpack' do
     app.deploy do
       expect(clean_output(app.output)).to include(<<~OUTPUT)
         remote:  !     Error: Gradle Wrapper is missing.
-        remote:  !     
+        remote:  !
         remote:  !     The Gradle Wrapper (gradlew) is required to build your application on Heroku.
         remote:  !     This ensures that your application builds with the same version of Gradle
         remote:  !     that you use during development. Projects are required to include
         remote:  !     their own Wrapper for reliable, reproducible builds.
-        remote:  !     
+        remote:  !
         remote:  !     Note: The buildpack used to provide a default Gradle Wrapper (using Gradle 2.0) for
         remote:  !     applications that didn't include their own wrapper. That workaround was deprecated
         remote:  !     in 2014 and has now been removed.
-        remote:  !     
+        remote:  !
         remote:  !     To fix this issue, run this command in your project directory
         remote:  !     locally and commit the generated files:
         remote:  !     $ gradle wrapper
-        remote:  !     
+        remote:  !
         remote:  !     If you don't have Gradle installed locally, you can install it first:
         remote:  !     https://gradle.org/install/
-        remote:  !     
+        remote:  !
         remote:  !     For more information about Gradle Wrapper, see:
         remote:  !     https://docs.gradle.org/current/userguide/gradle_wrapper.html
         remote:  !     https://devcenter.heroku.com/articles/deploying-gradle-apps-on-heroku
@@ -49,27 +49,27 @@ RSpec.describe 'Gradle buildpack' do
     app.deploy do
       expect(clean_output(app.output)).to include(<<~OUTPUT)
         remote:  !     Error: Failed to start Gradle daemon.
-        remote:  !     
+        remote:  !
         remote:  !     An error occurred while starting the Gradle daemon. This usually
         remote:  !     indicates an issue with the Gradle wrapper, build configuration,
         remote:  !     or system resources.
-        remote:  !     
+        remote:  !
         remote:  !     Check the output above for specific error messages from Gradle.
         remote:  !     Common causes include:
-        remote:  !     
+        remote:  !
         remote:  !     - Corrupted or missing Gradle wrapper files
         remote:  !     - Invalid Gradle configuration in build.gradle(.kts) or settings.gradle(.kts)
         remote:  !     - Network connectivity issues downloading Gradle dependencies
         remote:  !     - Incompatible Gradle version with the current Java runtime
-        remote:  !     
+        remote:  !
         remote:  !     To resolve this issue:
         remote:  !     1. Verify your Gradle wrapper works locally: ./gradlew --version
         remote:  !     2. Check your build.gradle(.kts) and settings.gradle(.kts) for syntax errors
         remote:  !     3. Ensure you're using a supported Gradle version
-        remote:  !     
+        remote:  !
         remote:  !     If this appears to be a temporary network or download issue,
         remote:  !     try deploying again as it may resolve itself.
-        remote:  !     
+        remote:  !
         remote:  !     For more information, see:
         remote:  !     https://docs.gradle.org/current/userguide/troubleshooting.html
       OUTPUT
@@ -95,25 +95,25 @@ RSpec.describe 'Gradle buildpack' do
     app.deploy do
       expect(clean_output(app.output)).to include(<<~OUTPUT)
         remote:  !     Error: Gradle build failed.
-        remote:  !     
+        remote:  !
         remote:  !     An error occurred during the Gradle build process. This usually
         remote:  !     indicates an issue with your application's dependencies, configuration,
         remote:  !     or source code.
-        remote:  !     
+        remote:  !
         remote:  !     First, check the build output above for specific error messages
         remote:  !     from Gradle that might indicate what went wrong. Common issues include:
-        remote:  !     
+        remote:  !
         remote:  !     - Missing or incompatible dependencies in your build.gradle(.kts)
         remote:  !     - Compilation errors in your application source code
         remote:  !     - Test failures (if tests are enabled during the build)
         remote:  !     - Invalid Gradle configuration or settings
         remote:  !     - Using an incompatible OpenJDK version for your project
-        remote:  !     
+        remote:  !
         remote:  !     To troubleshoot this issue:
         remote:  !     1. Try building your application locally with the same Gradle command
         remote:  !     2. Check that your gradlew script works locally: ./gradlew build
         remote:  !     3. Verify your Java version is compatible with your project
-        remote:  !     
+        remote:  !
         remote:  !     If the error persists, check the documentation:
         remote:  !     https://docs.gradle.org/current/userguide/troubleshooting.html
         remote:  !     https://devcenter.heroku.com/articles/deploying-gradle-apps-on-heroku
@@ -129,24 +129,24 @@ RSpec.describe 'Gradle buildpack' do
     app.deploy do
       expect(clean_output(app.output)).to include(<<~OUTPUT)
         remote:  !     Error: Gradle task 'pleasefail' not found.
-        remote:  !     
+        remote:  !
         remote:  !     The specified Gradle task 'pleasefail' does not exist in your project.
         remote:  !     This can happen when:
-        remote:  !     
+        remote:  !
         remote:  !     - The task name is misspelled
         remote:  !     - The task is defined in a plugin that hasn't been applied
         remote:  !     - The task is only available in certain project configurations
         remote:  !     - You're trying to run a task that doesn't exist in this project
-        remote:  !     
+        remote:  !
         remote:  !     To see all available tasks in your project, run locally:
         remote:  !     $ ./gradlew tasks
-        remote:  !     
+        remote:  !
         remote:  !     To see all tasks including those from plugins, run:
         remote:  !     $ ./gradlew tasks --all
-        remote:  !     
+        remote:  !
         remote:  !     If you're setting GRADLE_TASK as an environment variable, make sure
         remote:  !     it contains a valid task name for your project.
-        remote:  !     
+        remote:  !
         remote:  !     For more information about Gradle tasks, see:
         remote:  !     https://docs.gradle.org/current/userguide/more_about_tasks.html
       OUTPUT
@@ -158,20 +158,20 @@ RSpec.describe 'Gradle buildpack' do
     app.deploy do
       expect(clean_output(app.output)).to include(<<~OUTPUT)
         remote:  !     Warning: Unsupported Gradle version detected.
-        remote:  !     
+        remote:  !
         remote:  !     You are using Gradle 7.6.3, which is end-of-life and no longer
         remote:  !     receives security updates or bug fixes.
-        remote:  !     
+        remote:  !
         remote:  !     Please upgrade to Gradle 9 (current) for active support, or at minimum
         remote:  !     Gradle 8 for security fixes only.
-        remote:  !     
+        remote:  !
         remote:  !     This buildpack will attempt to build your application, but compatibility
         remote:  !     with unsupported Gradle versions is not guaranteed and may break in future
         remote:  !     buildpack releases. We strongly recommend upgrading.
-        remote:  !     
+        remote:  !
         remote:  !     For more information:
         remote:  !     - https://docs.gradle.org/current/userguide/feature_lifecycle.html#eol_support
-        remote:  !     
+        remote:  !
         remote:  !     Upgrade guides:
         remote:  !     - https://docs.gradle.org/current/userguide/upgrading_version_7.html
         remote:  !     - https://docs.gradle.org/current/userguide/upgrading_major_version_9.html
